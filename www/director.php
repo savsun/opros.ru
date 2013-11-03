673748b064b0bedf703377779d0602a7e1ee9265
FullBoa <egorogor@mail.ru>
2013-11-03 22:08:49 +0400

    Расчет матрицы рангов

--- www/director.php
+++ www/director.php
@@ -22,6 +22,7 @@
 			{
 				array_push($mark3,$row[0]);
 			}
+		print_r($mark3);
 		$query=mysql_query("SELECT mark FROM experttointerview WHERE numberOpros=1 and numberExpert=4");
 		$mark4=array();
 		while ($row = mysql_fetch_row($query))
@@ -29,6 +30,48 @@
 				array_push($mark4,$row[0]);
 			}
 		print_r($mark4);
+		
+		
+		$keyCount=count($mark3);
+		echo $keyCount;
+		$rankMatr=array();
+		for ($i=0; $i<(count($mark3)); $i++)
+			{
+				$rankRow=array();
+				for ($j=0; $j<(count($mark3)); $j++)
+				{
+					array_push($rankRow,0);
+				}
+				array_push($rankMatr,$rankRow);
+			} 
+		
+		for ($i=0; $i<(count($mark3)); $i++)
+			{
+				for ($j=$i+1; $j<(count($mark3)); $j++)
+				{
+					if ($mark3[$i]==$mark3[$j])
+					{
+						$rankMatr[$i][$j]=0;
+						$rankMatr[$j][$i]=0;
+					}
+					else if ($mark3[$i]>$mark3[$j])
+						{
+							$rankMatr[$i][$j]=1;
+							$rankMatr[$j][$i]=-1;
+						}
+						else
+						{
+							$rankMatr[$i][$j]=-1;
+							$rankMatr[$j][$i]=1;
+						}
+				}
+			}
+		echo "<br>";	
+		for ($i=0; $i<4; $i++)
+		{
+			print_r($rankMatr[$i]);
+			echo "<br>";
+		}
 	?>
 	
 </body>
