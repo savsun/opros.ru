9167d315349a8f341c565ddf31ffd35658cebf7a
FullBoa <egorogor@mail.ru>
2013-11-05 16:41:29 +0400

    Текущие изменения

--- www/director.php
+++ www/director.php
@@ -1,22 +1,71 @@
+
 <html>
  <head>
   <title>UserPage</title>
  </head>
  
-<body>
+<body bgcolor="ffc13d">
 	<center>
 		<h3>Здравствуйте, пользователь</h3>
 	</center>
-	<p>Посмотреть степени близости мнений экспертов в оконченных опросах</p>
+	<table valign="center" width="700" cellspacing="0" cellpadding="5">
+		<tr valign="left">
+			<td width="200">Посмотреть степени близости мнений экспертов в оконченных опросах<br>
+			<?php
+				$db=mysql_connect("localhost","root");
+				mysql_select_db ("interview");
+				$query=mysql_query("SELECT nameInterview FROM listofinterview WHERE dataEnding < NOW()");
+				echo "<form method='post' action='director.php'>";
+				while ($row = mysql_fetch_array($query))
+				{
+					echo "<input type='submit' name='".$row['nameInterview']."' value='".$row['nameInterview']."'/><br>";
+				}				
+				echo "</form>";
+				
+				
+			?>
+			</td>
+			<td>
+				<?php
+					$name=array_values($_POST);
+					echo "<center><h2>".$name[0]."</h2></center>";
+					
+					$result=mysql_query("SELECT id FROM listofinterview WHERE nameInterview='".$name[0]."'");
+					while ($row = mysql_fetch_row($result))
+					{
+						$id=$row[0];
+					}
+					
+					$query=mysql_query("SELECT COUNT(*) FROM users");
+					while ($row = mysql_fetch_row($query))
+					{
+						$count=$row[0];
+					}
+				
+					
+					$marks=array();
+					for ($i=3; $i<($count)+1; $i++)
+					{
+						$query=mysql_query("SELECT mark FROM experttointerview WHERE numberOpros=".$id." and numberExpert=".$i."");
+						if (mysql_num_rows ($query) != 0)
+						{
+							$mark=array();
+							while ($row = mysql_fetch_row($query))
+							{
+								array_push($mark,$row[0]);
+							}
+							/*print_r($mark);*/
+							array_push($marks,$mark);
+						}
+					}
+					/*print_r($marks);*/
+					
+				?>
+			</td>
+		</tr>
+	</table>
 	<?php
-			$db=mysql_connect("localhost","root");
-			mysql_select_db ("interview");
-			$query=mysql_query("SELECT nameInterview FROM listofinterview WHERE dataEnding < NOW()");
-			while ($row = mysql_fetch_array($query))
-			{
-				echo $row['nameInterview']."<br>";
-			}
-		$query=mysql_query("SELECT mark FROM experttointerview WHERE numberOpros=1 and numberExpert=3");
+/*$query=mysql_query("SELECT mark FROM experttointerview WHERE numberOpros=1 and numberExpert=3");
 		$mark3=array();
 		while ($row = mysql_fetch_row($query))
 			{
@@ -71,7 +120,7 @@
 		{
 			print_r($rankMatr[$i]);
 			echo "<br>";
-		}
+		}*/
 	?>
 	
 </body>
--- www/expert.php
+++ www/expert.php
@@ -6,21 +6,70 @@
   <title>ExpertPage</title>
  </head>
  
-<body>
+<body bgcolor="ffc13d">
 	<center>
 		<h3>Здравствуйте, 
 			<?php
 				$db=mysql_connect("localhost","root");
 				mysql_select_db ("interview");
-				$query=mysql_query("SELECT fio FROM users WHERE login=S_SESSION['login']");
-				echo $query;
+				$query=mysql_query("SELECT fio FROM users WHERE login='".$_SESSION['login']."'");
+				while ($row = mysql_fetch_row($query))
+					{
+						echo $row[0];
+					}
 			?>
 		</h3>
 	</center>
-	<p>Вам неободимо выставить оценки по следующим опросам</p>
-	<?php
-		$query=mysql_query("SELECT fio FROM users WHERE login=S_SESSION['login']");
-	?>
+	<table valign="center" width="700" cellspacing="0" cellpadding="5">
+		<tr valign="left">
+			<td width="400"> <h3>Вам неободимо выставить оценки<br> по следующим опросам</h3><br><br>
+			
+				<?php
+					
+					$query=mysql_query("SELECT userId FROM users WHERE login='".$_SESSION['login']."'");
+					while ($row = mysql_fetch_row($query))
+					{
+						$userId=$row[0];
+					}
+					$query=mysql_query("SELECT DISTINCT nameInterview FROM listofinterview JOIN experttointerview  ON listofinterview.id=experttointerview.numberOpros 
+													WHERE experttointerview.numberExpert='".$userId."' AND mark IS NULL") or die(mysql_error());
+					echo "<form method='post' action='expert.php'>";
+					while ($row = mysql_fetch_array($query))
+					{
+						echo "<input type='submit' name='".$row['nameInterview']."' value='".$row['nameInterview']."'/><br>";
+					}				
+					echo "</form>";			
+				?>
+			</td>
+			<td width="1000">
+			<?php
+				$name=array_values($_POST);
+				echo "<center><h2>".$name[0]."</h2></center><br><br>";
+				echo "<center>Поставьте оценку от 1 до 10 (10-самый лучший, 1-самый худший)</center><br><br>";
+				$query=mysql_query("SELECT measure FROM listofinterview JOIN experttointerview  ON listofinterview.id=experttointerview.numberOpros
+											WHERE experttointerview.numberExpert='".$userId."' AND listofinterview.nameInterview='".$name[0]."'");
+				echo "<form method='POST' action='expert.php'>";
+				while ($row = mysql_fetch_row($query))
+					{
+						echo $row[0]."<input type='text' name='".$row[0]."' required/><br><br>";
+					}
+				echo "<input type='submit' name='estimate' value='Оценить'/><br>";
+				echo "</form>";
+				var_dump($_POST);
+				
+				if (isset ($_POST['estimate']))
+					{
+						foreach($_POST as $key => $value) 
+							{ 
+								$query=mysql_query("UPDATE experttointerview SET mark='".$value."' WHERE measure='".str_replace('_',' ',$key)."'")or die(mysql_error());
+							} 
+					
+							
+					}
+			?>
+			</td>
+		</tr>
+	</table>
 	
 </body>
 </html>
